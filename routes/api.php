b53a48f76f3a116d05561b7c30c4cb1c1f2df14c
M1CH34l <trillzglobal@gmail.com>
2022-04-22 06:56:28 +0100

    Frontend URLs and Bulk Airtime url

--- routes/api.php
+++ routes/api.php
@@ -24,12 +24,17 @@ Route::group(['prefix'=>'bulkrecharge/v1'],function($router){
         Route::get('sample/upload',);
         Route::post('upload/data',"\App\Http\Controllers\Service\VendController@uploadData");
         Route::get('query/{reference}',"\App\Http\Controllers\Service\TransactionController@queryTransaction");
+        Route::post('upload/airtime',"\App\Http\Controllers\Service\VendController@uploadAirtime");
     });
     Route::post('create/ipaddress',"\App\Http\Controllers\User\IpDataController@storeIpData");
     Route::post('register',"\App\Http\Controllers\User\RegistrationController@register");
     Route::post('access/token', "\App\Http\Controllers\User\UserController@login");
     Route::get('cron/request', '\App\Http\Controllers\Service\TransactController@CronTransact');
-
+    Route::group(['middleware'=>['auth:api']], function($router){
+        Route::get("statistics", "\App\Http\Controllers\Service\TransactionController@getStatistics");
+        Route::get("transactions", "\App\Http\Controllers\Service\TransactionController@getTransactions");
+        Route::get("transaction/{reference}", "\App\Http\Controllers\Service\TransactionController@queryTransaction");
+    });
     Route::post('download/data_package', '\App\Http\Controllers\Service\VendController@get_data_package');
     Route::post('download/sample_data', '\App\Http\Controllers\Service\VendController@get_sample_data');
 });
